57315b67d52bd8a77c55378963d421a88e53de0d
James Noss <jnoss@jhu.edu>
2024-04-09 12:09:48 -0400

    Add Connection interface layer
    Signed-off-by: James Noss <jnoss@jhu.edu>

---
+++ evolver/connection/interface.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+from threading import RLock
+
+import pydantic
+
+
+class Connection(ABC):
+    """ Interface for a connection protocol, abstracting and wrapping lower-level communication over self.backend. """
+
+    class Config(pydantic.BaseModel):
+        ...
+
+    backend = None  # Backend module/library to use.
+
+    def __init__(self, *args, config: Config = None, lock_constructor=RLock, **kwargs):
+        self.conn = None  # This is the core object that this class wraps.
+        self.config = config or self.Config()
+        self.lock = lock_constructor()
+
+    @abstractmethod
+    def _open(self,  *args, **kwargs):
+        """ Open a connection using self.backend and return connection instance to be assigned to self.conn by
+            self.open(). See self.open() & self.__enter__().
+        """
+        ...
+
+    @abstractmethod
+    def _close(self, *args, **kwargs):
+        """ Close the connection using either self.backend or self.conn. See self.close() & self.__exit__(). """
+        ...
+
+    @abstractmethod
+    def read(self, *args, **kwargs):
+        """ Implement read protocol. """
+        ...
+
+    @abstractmethod
+    def write(self, *args, **kwargs):
+        """ Implement write protocol. """
+        ...
+
+    @abstractmethod
+    def communicate(self, *args, **kwargs):
+        """ Implement write/read protocol all in one. """
+        ...
+
+    def open(self, *args, reuse=True, **kwargs):
+        if not reuse:
+            self.close()
+
+        if self.conn is None:
+            self.conn = self._open(*args, **kwargs)
+        return self.conn
+
+    def close(self, *args, **kwargs):
+        if self.conn:
+            try:
+                return self._close(*args, **kwargs)
+            finally:
+                # NOTE: We assume here that a failure when closing renders the connection undefined so nullify.
+                self.conn = None
+
+    def __enter__(self):
+        self.lock.acquire()
+        try:
+            self.open()
+        except Exception:
+            self.lock.release()
+            raise
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.close()
+        finally:
+            self.lock.release()
