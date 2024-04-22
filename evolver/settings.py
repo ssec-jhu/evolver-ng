c18e727aa61f40c34b3c1cbd4f355d5051dbe46b
James Noss <jnoss@jhu.edu>
2024-04-22 14:35:26 -0400

    Use pydantic-settings framework
    Signed-off-by: James Noss <jnoss@jhu.edu>

---
+++ evolver/settings.py
@@ -0,0 +1,6 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="EVOLVER_",
+                                      case_sensitive=True)
