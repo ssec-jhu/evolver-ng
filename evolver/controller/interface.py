23d95f85ef3d8f54a736922c11dbc38f5e2ed54d
James Noss <jnoss@jhu.edu>
2024-04-30 13:20:06 -0400

    Remove evolver requirement from Controller
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- evolver/controller/interface.py
+++ evolver/controller/interface.py
@@ -1,27 +1,13 @@
 from abc import ABC, abstractmethod
 
-from evolver.base import BaseConfig, ConfigDescriptor
+from evolver.base import BaseConfig
 
 
 class Controller(ABC):
-    """
-        Base interface class for all control implementations.
-
-        Attributes:
-            devices (:obj:`dict` of :obj:`Device`, :obj:`dict` of :obj:`ConfigDescriptor`): Dictionary of devices to
-                control.
-    """
+    """ Base interface class for all control implementations. """
 
     class Config(BaseConfig):
-        devices: dict
-
-    def __init__(self, devices: dict):
-        self.devices = devices
-
-        # Instantiate objects from config descriptors.
-        for device in self.devices:
-            if isinstance(device, ConfigDescriptor):
-                self.devices[device] = device.create()
+        ...
 
     def pre_control(self, *args, **kwargs):
         """ Hook for customization pre-control execution, see self.run().
