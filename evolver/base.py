92e2719957d36a5256857487d5432819a6556539
James Noss <jnoss@jhu.edu>
2024-06-13 14:01:13 -0400

    Load config from file using model_validate
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- evolver/base.py
+++ evolver/base.py
@@ -1,5 +1,6 @@
 import logging
 from abc import ABC
+import os
 from pathlib import Path
 from typing import Annotated, Any, Dict
 
@@ -53,6 +54,8 @@ class _BaseConfig(pydantic.BaseModel):
         """
         if obj is None:
             return cls()
+        elif isinstance(obj, os.PathLike):
+            return cls.load(file_path=obj)
         elif isinstance(obj, (str, bytes, bytearray)):
             return cls.model_validate_json(obj, strict=strict, context=context)
         elif isinstance(obj, ConfigDescriptor):
