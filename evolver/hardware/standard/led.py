8b113c0d0ee8affc1739b9d8f92d83342fbba5b6
Arik Mitschang <arik@jhu.edu>
2024-07-02 14:13:37 -0400

    LED driver (#96)
    Add LED driver implementation. This version takes brightness which is directly proportional to 
    PWM duty cycle - so no calibration necessary.

---
+++ evolver/hardware/standard/led.py
@@ -0,0 +1,36 @@
+from copy import copy
+
+from pydantic import Field
+
+from evolver.hardware.interface import EffectorDriver, VialBaseModel
+from evolver.hardware.standard.base import SerialDeviceConfigBase
+from evolver.serial import SerialData
+
+
+class LED(EffectorDriver):
+    class Config(SerialDeviceConfigBase):
+        pwm_max: int = 4095
+        default_brightness: float = 1.0
+
+    class Input(VialBaseModel):
+        brightness: float = Field(1.0, ge=0, le=1, description="brightness on scale of 0 to 1")
+
+    @property
+    def serial(self):
+        return self.serial_conn or self.evolver.serial
+
+    def commit(self):
+        inputs = copy(self.committed)
+        inputs.update({v: i for v, i in self.proposal.items() if v in self.vials})
+
+        def from_brightness(brightness):
+            # no calibration needed, the brightness range just comes from time-on via
+            # PWM duty cycle
+            return str(min(self.pwm_max, max(0, int(self.pwm_max * brightness)))).encode()
+
+        cmd = [from_brightness(self.default_brightness)] * self.slots
+        for v, i in inputs.items():
+            cmd[v] = from_brightness(i.brightness)
+        with self.serial as comm:
+            comm.communicate(SerialData(addr=self.addr, data=cmd))
+        self.committed = inputs
