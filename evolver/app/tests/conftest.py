56ac46746b3411e69f64a5f43aaaa53d49753619
James Noss <jnoss@jhu.edu>
2024-08-02 17:41:55 -0400

    Add default config
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- evolver/app/tests/conftest.py
+++ evolver/app/tests/conftest.py
@@ -2,16 +2,9 @@ import pytest
 from fastapi.testclient import TestClient
 
 from evolver.app.main import app
-from evolver.device import Evolver
-from evolver.settings import app_settings
 
 
 @pytest.fixture
 def app_client(tmp_path, monkeypatch):
-    monkeypatch.setattr(app_settings, "CONFIG_FILE", tmp_path / app_settings.CONFIG_FILE)
-
-    # Create and save a default config file to be read upon app startup.
-    Evolver.Config().save(app_settings.CONFIG_FILE)
-
     with TestClient(app) as client:
         yield client
