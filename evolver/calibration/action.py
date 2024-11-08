342e3eddc920ec0d821de902206b70097021a159
iain maitland <github@iainmaitland.com>
2024-11-08 14:01:42 -0500

    update CalibrationAction with docstring

--- evolver/calibration/action.py
+++ evolver/calibration/action.py
@@ -5,11 +5,15 @@ from pydantic import BaseModel
 
 
 class CalibrationAction(ABC):
-    name: str
-    description: str
-    requires_input: bool
-
     def __init__(self, name: str, description: str, requires_input: bool = False):
+        """
+        Initialize a CalibrationAction.
+
+        Args:
+            name (str): The name of the calibration action, used to identify it within a calibration procedure composed of many actions.
+            description (str): A short description of the action's purpose, useful for documentation or display to the person performing a calibration procedure composed of actions.
+            requires_input (bool): Indicates whether this action requires user input (default is False) actions that require input should have a FormModel defined to express the shape of the required user input.
+        """
         self.name = name
         self.description = description
         self.requires_input = requires_input
