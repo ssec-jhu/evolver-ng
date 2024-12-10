ed1c9b3f98a1c225806d9a6836d970bc5a477277
Arik Mitschang <arik@jhu.edu>
2024-12-10 12:21:35 -0500

    add docstring

--- evolver/logutils.py
+++ evolver/logutils.py
@@ -8,6 +8,19 @@ logging.addLevelName(EVENT, "EVENT")
 
 
 class LogInfo:
+    """Helper class to provide "extra" to log records integrated with history.
+
+    This class acts similar to a dict, but with all fields within a dict under a
+    specific key and ensures the fields are json-serializable. Used with the
+    history integration, fields provided will be available in the stored record.
+    This is useful because all fields within extra are placed at the top level
+    of a logrecord dict, so otherwise there is no way to distinguish them.
+
+    Example:
+        self.logger.log(EVENT, "Event message", extra=LogInfo(vial=1))
+        self.logger.info(".", extra={"not_stored": 1, **LogInfo(vial=1).dump()})
+    """
+
     EXTRA_KEY = "evolver_extra"
 
     @staticmethod
@@ -28,13 +41,13 @@ class LogInfo:
     def dump(self):
         return {self.EXTRA_KEY: self._dict}
 
-    def __iter__(self):
+    def __iter__(self):  # to integrate with logging, it inspects keys, then gets items.
         def iter():
             yield self.EXTRA_KEY
 
         return iter()
 
-    def __getitem__(self, _):
+    def __getitem__(self, _):  # for logging, as above.
         return self._dict
 
 
