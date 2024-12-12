0d416fb5428ca02faa8df3c8989da545b1c09ce3
Arik Mitschang <arik@jhu.edu>
2024-12-12 16:33:49 -0500

    Log pass through user supplied extra data (#226)
    This change adds a helper utility to add extra evolver-specific context info to logs (e.g. vial) that will then be passed on to the history endpoint.

--- evolver/logutils.py
+++ evolver/logutils.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from evolver.settings import settings
@@ -6,19 +7,62 @@ EVENT = logging.INFO + 1
 logging.addLevelName(EVENT, "EVENT")
 
 
+class LogInfo:
+    """Helper class to provide "extra" to log records integrated with history.
+
+    This class acts similar to a dict, but with all fields within it "keyed" by
+    an EXTRA_KEY that is stripped when the log event is read. This has the
+    effect of normalizing LogInfo data, so that when it is read it appears at
+    the same level as the Log entry's top-level properties.
+
+    Note:
+        This is useful because all fields within extra are placed at the top
+        level of a logrecord dict, so otherwise there is no way to distinguish
+        them.
+
+    Example:
+        self.logger.log(EVENT, "Event message", extra=LogInfo(vial=1))
+        self.logger.info(".", extra={"not_stored": 1, **LogInfo(vial=1).dump()})
+    """
+
+    EXTRA_KEY = "evolver_extra"
+
+    @staticmethod
+    def json_check(data, silent=False):
+        if data == {}:
+            return data
+        try:
+            json.dumps(data)
+            return data
+        except TypeError as e:
+            if silent:
+                return {}
+            raise ValueError(f"Extra data must be json-serializable, got error: {e}")
+
+    def __init__(self, **kwargs):
+        self._dict = __class__.json_check(kwargs)
+
+    def dump(self):
+        return {self.EXTRA_KEY: self._dict}
+
+    def __iter__(self):  # to integrate with logging, it inspects keys, then gets items.
+        def iter():
+            yield self.EXTRA_KEY
+
+        return iter()
+
+    def __getitem__(self, _):  # for logging, as above.
+        return self._dict
+
+
 class LogHistoryCaptureHandler(logging.Handler):
     def __init__(self, history_server):
         super().__init__()
         self.history_server = history_server
 
     def emit(self, record):
-        vial = getattr(record, "vial", None)
-        record_dict = {"level": record.levelname, "message": record.getMessage()}
-        # TODO: add support for some extra fields (but these should be
-        # json-seralizable). This might come in the form of named extra data,
-        # expected to be a jsonable dict, e.g. extra={"vial": 1, "extra": ...},
-        # or we could attempt to serialize other fields and include those that
-        # succeed (maybe outside known ones).
+        extra = LogInfo.json_check(getattr(record, LogInfo.EXTRA_KEY, {}), silent=True)
+        record_dict = {"level": record.levelname, "message": record.getMessage(), **extra}
         kind = "event" if record.levelno == EVENT else "log"
         # we strip the package name from logger name since for history logs it
         # is redundant (used here for routing purposes) and without it we can
@@ -26,4 +70,4 @@ class LogHistoryCaptureHandler(logging.Handler):
         name = record.name
         if name.startswith(f"{settings.DEFAULT_LOGGER}."):
             name = name[len(__package__) + 1 :]
-        self.history_server.put(name, kind, record_dict, vial=vial)
+        self.history_server.put(name, kind, record_dict, vial=record_dict.get("vial", None))
