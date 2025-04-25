2af4a3e34f11690014ac2df9ee31f927cab04955
iain maitland <github@iainmaitland.com>
2025-04-25 14:39:23 -0400

    Iainland/procedure perf bug (#298)
    * fix performance of procedure
    * also stops procedure_file getting too massive (it was getting to be 5mb)
    
    pi@raspberrypi:~/evolver-ui $ ls -ltrh /calibration_files/Temperature_2025_04_25T19_13_25.yml 
    -rw-r--r-- 1 root root 17K Apr 25 19:15 /calibration_files/Temperature_2025_04_25T19_13_25.yml

--- evolver/app/tests/test_calibration.py
+++ evolver/app/tests/test_calibration.py
@@ -364,7 +364,7 @@ def test_get_calibration_data(tmp_path):
             {"completed_actions": [], "history": [], "measured": {}, "started": True},
             {
                 "completed_actions": ["measure_vial_0_temperature"],
-                "history": [{"completed_actions": [], "history": [], "measured": {}, "started": True}],
+                "history": [],
                 "measured": {"0": {"raw": [], "reference": [25.0]}},
                 "started": True,
             },
@@ -372,7 +372,6 @@ def test_get_calibration_data(tmp_path):
         "measured": {"0": {"raw": [1.23], "reference": [25.0]}},
         "started": True,
     }
-
     assert actual_state == expected_state
 
 
