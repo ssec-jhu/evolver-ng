2f537f8e75730a109c928275e7be474b7b8327ce
Arik Mitschang <arik@jhu.edu>
2025-05-27 14:24:47 -0400

    make controller serializable (#307)
    Controllers must be serializable via the baseinterface mechanism. This fixes the chemostat and adds a helper to enable contollers to easily take either str-references to evolver hardware or hardware instances. Adds also test for such serializability.

--- evolver/tests/test_controller.py
+++ evolver/tests/test_controller.py
@@ -1,4 +1,9 @@
+from unittest.mock import MagicMock
+
+import pytest
+
 from evolver.controller.interface import Controller
+from evolver.device import Evolver
 
 
 class TestController(Controller):
@@ -17,3 +22,17 @@ class TestController(Controller):
 class TestControllerInterface:
     def test_hooks(self):
         assert TestController(evolver=None).run() == "pre control data, control data, post control data"
+
+    def test_get_hw(self):
+        sensor_mock = MagicMock()
+        evolver = Evolver(hardware={"sensor": sensor_mock})
+        controller = TestController(evolver=evolver)
+        # for strings we expect the hardware attached to evolver manager
+        assert controller.get_hw("sensor") == sensor_mock
+        with pytest.raises(KeyError):
+            controller.get_hw("nonexistent_sensor")
+
+        # for hardware passed directly, we return the same object and do not
+        # consult the evolver manager
+        direct_hw_mock = MagicMock()
+        assert controller.get_hw(direct_hw_mock) == direct_hw_mock
