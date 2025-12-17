41cb8de9672bb744603864205ece18c9ba53f2c8
iain maitland <github@iainmaitland.com>
2025-12-17 14:02:27 -0500

    delete stuff

--- test_script.py
+++
@@ -1,8 +0,0 @@
-from evolver.calibration.standard.calibrators.temperature import TemperatureCalibrator
-from evolver.device import Evolver
-from evolver.hardware.standard.temperature import Temperature
-
-# calibrator=None
-calibrator = TemperatureCalibrator()
-x = Evolver(hardware={"test": Temperature(addr="x", calibrator=calibrator)}).config_model
-print(x)
